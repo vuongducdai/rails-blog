0eafc788f8d44eb72242c0f3358ffbd68a04f7f5
daivd <daivuong7696@gmail.com>
2024-10-02 20:47:42 +0700

    GET a blog post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # as: blog_post will generate blog_post_path and blog_post_url method that 
+  # we can access in the views
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
