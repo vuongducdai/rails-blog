d34bf1fe60aef924310b5064aa0ac0cf8f81528e
daivd <daivuong7696@gmail.com>
2024-10-03 06:51:07 +0700

    create and insert to database

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # as: blog_post will generate blog_post_path and blog_post_url method that 
   # we can access in the views
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
